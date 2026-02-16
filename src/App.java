b86d727b32e66790b6ca893de3d3b57edda467f1
Assim Alaslani <assimaslani136@gmail.com>
2026-02-16 14:49:42 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,20 +1,58 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
         String[] name = new String[1];
-        int[] phone = new int[1];
+        String[] phone = new String[1];
+        char op;
+
+        for(int i = 0; i < name.length; i++) {
+            System.out.println("Enter your name :");
+            name[i] = input.nextLine();
+
+        }
+
+        for(int a = 0; a < phone.length; a++) {
+            System.out.println("Enter phone number :");
+            phone[a] = input.nextLine();
+
+            if (phone[a].matches("\\d{10}")) {
+                System.out.println("Valid phone");
+            } else {
+                System.out.println("Invalid phone");
+                return;
+            }
+            
+
+        }
+
+        System.out.println("Select case type");
+        System.out.println("1 - Emergency situation\n" + "\n" +  "2 - Normal situation");
+        op = input.nextLine().charAt(0);
+
+        switch (op) {
+            case '1':
+                System.out.println("sss");
+                break;
+
+            case '2':
+                System.out.println("2222");
+                break;
+        }
+
+        
         
 
 
 
-        for(int i = 0; i < name.length; i++) {
-            String input = JOptionPane.showInputDialog("Enter your Full name");
-            String input1 = JOptionPane.showInputDialog("Enter your Mobile number");
-            phone[i] = Integer.parseInt(input1);
 
-        }
+        
+
+       
 
         
     }
